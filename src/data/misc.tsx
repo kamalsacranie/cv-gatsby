d207e069f9e47ce58469571069fc61935b2b172a
Kamal Sacranie <kamalsjunkmail@gmail.com>
2022-10-03 20:18:24 +0100

    Change data to javascript instead of json

---
+++ src/data/misc.tsx
@@ -0,0 +1,10 @@
+// Is this how you typescript?? lol
+type Subheading = {
+    code: boolean;
+    text: string;
+};
+
+export const subheading: Subheading = {
+    code: true,
+    text: "render_cv(*args, **kwargs)",
+};
---
+++ src/data/personal.tsx
@@ -0,0 +1,32 @@
+export const names = {
+  first_name: "Kamal",
+  last_name: "Sacranie",
+};
+
+export const contact_details = {
+  phone: {
+    icon: "VscGithubInverted",
+    : "+47951642005",
+        
+            : {
+                scAdd",
+                ithub.com/kamalsacranie",
+                    
+                        
+                            ,
+                                work@gmail.com",
+                            
+                        
+                    Book",
+                amalsacranie.github.io",
+                
+                    
+                
+                    
+                    
+                      
+                      
+                
+              
+        
+    
