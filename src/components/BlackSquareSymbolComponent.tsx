8a9e9b323cb704031cafb1924b2400ff52424d19
Kamal Sacranie <kamalsjunkmail@gmail.com>
2022-10-03 23:01:57 +0100

    Fix heading part of CV

--- src/components/BlackSquareSymbolComponent.tsx
+++ src/components/BlackSquareSymbolComponent.tsx
@@ -2,10 +2,13 @@ import React, { PropsWithChildren } from "react";
 import * as icons from "react-icons/vsc";
 
 // TODO make it dynamic for all the types of icons so we can import react-icons/vsc
-const BlackSquareSymbol = ({ name }: PropsWithChildren<String>) => {
+const BlackSquareSymbol = ({
+    name,
+    extend_style,
+}: PropsWithChildren<String>) => {
     return (
-        <div className="bg-black text-white self-center p-3">
-            {React.createElement(icons[name])}
+        <div className={`bg-black text-white self-center p-2 ${extend_style}`}>
+            {icons[name] && React.createElement(icons[name])}
         </div>
     );
 };
--- src/components/ContactGridComponent.tsx
+++ src/components/ContactGridComponent.tsx
@@ -5,12 +5,12 @@ import BlackSquareSymbol from "./BlackSquareSymbolComponent";
 const ContactGrid = () => {
     return (
         <div>
-            <div className="grid grid-cols-2 gap-2">
+            <div className="grid grid-cols-2 gap-2 text-xs">
                 {Object.values(contact_details).map(({ data, icon }) => {
                     return (
-                        <div className="flex">
-                            <BlackSquareSymbol name={icon} />
-                            {data}
+                        <div className="flex align-middle">
+                            <BlackSquareSymbol name={icon} extend_style="mr-1" />
+                            <span className="self-center">{data}</span>
                         </div>
                     );
                 })}
