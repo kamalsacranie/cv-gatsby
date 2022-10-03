a221039173bf08a1c08783745b597521435fb6e4
Kamal Sacranie <kamalsjunkmail@gmail.com>
2022-10-03 20:35:48 +0100

    fixed weird format error

--- src/data/personal.tsx
+++ src/data/personal.tsx
@@ -1,32 +1,23 @@
 export const names = {
-  first_name: "Kamal",
-  last_name: "Sacranie",
+    first_name: "Kamal",
+    last_name: "Sacranie",
 };
 
 export const contact_details = {
-  phone: {
-    icon: "VscGithubInverted",
-    : "+47951642005",
-        
-            : {
-                scAdd",
-                ithub.com/kamalsacranie",
-                    
-                        
-                            ,
-                                work@gmail.com",
-                            
-                        
-                    Book",
-                amalsacranie.github.io",
-                
-                    
-                
-                    
-                    
-                      
-                      
-                
-              
-        
-    
+    phone: {
+        icon: "VscGithubInverted",
+        data: "+47951642005",
+    },
+    github: {
+        icon: "VscAt",
+        data: "github.com/kamalsacranie",
+    },
+    email: {
+        icon: "VscBug",
+        data: "sacranie.work@gmail.com",
+    },
+    website: {
+        icon: "VscAt",
+        data: "kamalsacranie.github.io",
+    },
+};
