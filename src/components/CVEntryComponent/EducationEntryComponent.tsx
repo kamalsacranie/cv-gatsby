83008c02897cc9050a6a014bc91d4b28de7fe981
Kamal Sacranie <kamalsjunkmail@gmail.com>
2022-10-11 10:50:44 +0100

    Update education entry component

--- src/components/CVEntryComponent/EducationEntryComponent.tsx
+++ src/components/CVEntryComponent/EducationEntryComponent.tsx
@@ -2,17 +2,15 @@ import React from "react";
 import Markdown from "../MarkdownComponent";
 
 const EducationEntry = ({ entry }) => {
-  return (
-    <div className="grid grid-cols-7 gap-x-4 py-2">
-      <div className="col-span-1">
-        <Markdown className="bg-black text-white right-0" source={entry.date} />
-      </div>
-      <div className="col-span-6">
-        <Markdown source={entry.lineOne} />
-        <Markdown source={entry.lineTwo} />
-      </div>
-    </div>
-  );
+    return (
+        <div className="py-2">
+            <div className="flex justify-between">
+                <Markdown source={entry.lineOne} />
+                <Markdown className="bg-black text-white right-0" source={entry.date} />
+            </div>
+            <Markdown source={entry.lineTwo} />
+        </div>
+    );
 };
 
 export default EducationEntry;
